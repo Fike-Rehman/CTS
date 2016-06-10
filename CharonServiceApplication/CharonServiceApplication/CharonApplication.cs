a496592defb312ae43a6e9d0128c9cbcef6aafd8
frehman <frehman@NCM21877LT>
2016-06-10 16:47:57 -0500

    Moving with adding a ping timer

--- CharonServiceApplication/CharonServiceApplication/CharonApplication.cs
+++ CharonServiceApplication/CharonServiceApplication/CharonApplication.cs
@@ -12,6 +12,9 @@ namespace CTS.Charon.CharonApplication
         private static readonly log4net.ILog _logger =
                  log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
+        private Timer _pingtimer;
+
+
         public CharonApplication(bool consoleMode)
         {
             _consoleMode = consoleMode;
@@ -25,7 +28,31 @@ namespace CTS.Charon.CharonApplication
             else
                 _logger.Info($"Started Charon Service in console mode {DateTime.Now}");
 
-            ExecuteDevicePing();
+            
+            // Initialize and execute a device Ping to see if our board is online:
+            var pingTask = new DevicePingTask();
+
+            if (pingTask.Execute(LogMessage))
+            {
+                // Device initialization succeeded. We can continue with more operations:
+                var pingInterval = new TimeSpan(0, 0, 1, 0); // 1 minute
+                // set up a timer that sends a ping asynchronously every minute:
+                _pingtimer = new System.Threading.Timer(OnPingTimer, null, pingInterval, Timeout.InfiniteTimeSpan);
+            }
+            else
+            {
+                // introduce a delay to give it a chance to report the progress:
+                Thread.Sleep(1000);
+
+                LogMessage($"Device Ping Failed after {pingTask.NumTries} attempts");
+
+                // There is not much point in continuing on at this point. Just send
+                // out an Alert and stop the app:
+                LogMessage("Device is either not online or has mal-functioned.");
+                LogMessage("Sending Alert...");
+
+            }
+
 
             if (!_consoleMode)
             {
@@ -37,35 +64,15 @@ namespace CTS.Charon.CharonApplication
             Stop();
         }
 
-        private void ExecuteDevicePing()
+        private void OnPingTimer(object state)
         {
-            //var numRetries = 3;
+            // send a ping asynchronously and reset the timer
 
-            //TODO: elevate this to class variable and call execute repeatedly
-            var pingTask = new DevicePingTask();
-
-            //var n = 0;
-             
-            //while(n < numRetries)
-            //{
-            //    n++;
-
-            //    // This will be blocking! It might cause problems if we running in a ASP.NET context
-            //    https://msdn.microsoft.com/en-us/magazine/jj991977.aspx
-            //    if (pingTask.ExecuteAsync(new Progress<string>(LogMessage)).Result)
-            //    {
-            //        return;
-            //    }
-            //}
-
-            pingTask.Execute(LogMessage);
-
-            // introduce a delay to give it a chance to report the progress:
-            Thread.Sleep(1000);
-
-            LogMessage($"Device Ping Failed after {pingTask.NumTries} attempts");
+            var pingInterval = new TimeSpan(0, 0, 1, 0); // 1 minute
+            _pingtimer.Change(pingInterval, Timeout.InfiniteTimeSpan);
         }
 
+        
         private void LogMessage(string msg )
         {
             if (_consoleMode)
