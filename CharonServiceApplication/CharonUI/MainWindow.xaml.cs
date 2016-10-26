b9272c7f9de08d02eb18ef163ec8b38186be9d78
Fike Rehman <fike.rehman@gmail.com>
2016-10-25 20:24:26 -0500

    Hooked up btn events in the Charon UI

--- CharonServiceApplication/CharonUI/MainWindow.xaml.cs
+++ CharonServiceApplication/CharonUI/MainWindow.xaml.cs
@@ -1,36 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using CTS.Charon.Devices;
 using System.Configuration;
 using System.Windows.Threading;
 using CTS.Common.Utilities;
 
+
 namespace CharonUI
 {
-    
-
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-       // private static readonly NetDuinoPlus _netDuino;
+        private readonly NetDuinoPlus netDuino;
 
         public MainWindow()
         {
             InitializeComponent();
+
 
             DispatcherTimer timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromSeconds(1);
@@ -46,6 +35,21 @@ namespace CharonUI
             lblSunriseVal.Content = sunriseToday.ToShortTimeString();
             lblSunsetVal.Content = sunsetToday.ToShortTimeString();
 
+
+            // We dont really have a way to get the current state of the AC & DC bus
+            // So we just initialize the switches to 'off' state which may not be the true 
+            // state of the Netduino.
+
+            BtnDCBus.IsChecked = false;
+            BtnACBus.IsChecked = false;
+
+            BtnDCBus.Checked += BtnDCBus_Checked;
+            BtnDCBus.Unchecked += BtnDCBus_Unchecked;
+
+            BtnACBus.Checked += BtnACBus_Checked;
+            BtnACBus.Unchecked += BtnACBus_Unchecked;
+
+
             // Read in the configurtion:
             var deviceIP = string.Empty;
 
@@ -59,26 +63,46 @@ namespace CharonUI
                 MessageBox.Show("Error Reading Configuration File");
             }
 
-            // talk to this device and get the current status of the relays:
-          //  _netDuino = NetDuinoPlus.Instance(deviceIP);
+            
+            netDuino = NetDuinoPlus.Instance(deviceIP);        
+        }
 
-           // InitButtonStates();
+        private void BtnACBus_Unchecked(object sender, RoutedEventArgs e)
+        {
+            // turn the lights off
+            MessageBox.Show("turned the AC Bus off");
+
+            // SetACRelayAsync(false);
+        }
+
+        private void BtnACBus_Checked(object sender, RoutedEventArgs e)
+        {
+            // turn the lights On
+            MessageBox.Show("turned the AC Bus oN");
+
+            // SetACRelayAsync(true);
+        }
+
+        private void BtnDCBus_Unchecked(object sender, RoutedEventArgs e)
+        {
+            // turn the lights off
+            MessageBox.Show("turned the DC Bus off");
+
+           // SetDCRelayAsync(false);
+
+        }
+
+        private void BtnDCBus_Checked(object sender, RoutedEventArgs e)
+        {
+            // trun the lights oN
+            MessageBox.Show("turned the DC Bus oN");
+
+            // SetDCRelayAsync(true);
         }
 
         private void Timer_Tick(object sender, EventArgs e)
         {
             lblDate.Text = DateTime.Now.ToLongDateString() + " " + DateTime.Now.ToLongTimeString();
-        }
-
-        private void OnTurnOn(object sender, RoutedEventArgs e)
-        {
-            
-            
-        }
-
-        private void OnTurnOff(object sender, RoutedEventArgs e)
-        {
-
         }
 
         protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
@@ -94,19 +118,32 @@ namespace CharonUI
             this.Close();
         }
 
+        private async void SetDCRelayAsync(bool on)
+        {
+            string result;
 
-        //private void InitButtonStates()
-        //{
-        //   if(NetDuinoPlus.IsRelay1Energized)
-        //   {
-        //        // means the DC Bus is  off. Slide the button to off position:
-        //        BtnDCBus.IsChecked = false;
-        //   }
-        //   else
-        //   {
-        //        BtnDCBus.IsChecked = true;
-        //   }
+            // Denergize the relay to turn the light on, otherwise enrgize them
 
-        //}
+            result = @on ? await netDuino.DenergizeRelay1() : await netDuino.EnergizeRelay1();
+
+            if (result == "Success")
+            {
+                // announce success
+            }
+        }
+
+        private async void SetACRelayAsync(bool on)
+        {
+            string result;
+
+            // Denergize the relay to turn the light on, otherwise enrgize them
+
+            result = @on ? await netDuino.DenergizeRelay2() : await netDuino.EnergizeRelay2();
+
+            if (result == "Success")
+            {
+                // announce success
+            }
+        }
     }
 }
